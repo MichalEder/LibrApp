b65fa87be960a5e08764f9e14be0a9bfbbc86093
MichalEder <edermichal.eder@gmail.com>
2023-09-11 09:55:32 +0200

    pridan form pro vyhledani dle isbn

--- dj_Librapp/Librapp/forms.py
+++ dj_Librapp/Librapp/forms.py
@@ -5,5 +5,8 @@ class KnihaForm(forms.ModelForm):
 
     class Meta:
         model = Kniha
-        fields = ['nazev', 'autor', 'rok_vydani', 'zanr', 'jazyk', 'ISBN10', 'ISBN13'] #'majitel']
+        fields = ['nazev', 'autor', 'rok_vydani', 'zanr', 'jazyk', 'ISBN10', 'ISBN13','majitel']
+
+class VyhledavaniISBNForm(forms.Form):
+    isbn = forms.CharField(label='ISBN', max_length=13, required=False)
 
