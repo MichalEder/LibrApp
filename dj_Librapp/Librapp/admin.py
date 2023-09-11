ae176f3d3e20caa806f48dde567dded17ffc7c38
MichalEder <edermichal.eder@gmail.com>
2023-09-11 09:11:46 +0200

    registrace modelu

--- dj_Librapp/Librapp/admin.py
+++ dj_Librapp/Librapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Kniha, Zanr, Uzivatel
 # Register your models here.
+admin.site.register(Kniha)
+admin.site.register(Zanr)
+admin.site.register(Uzivatel)
