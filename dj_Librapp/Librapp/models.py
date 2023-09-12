ed494954a15a71f679c2d7dc5113d4fb850221e7
MichalEder <edermichal.eder@gmail.com>
2023-09-12 11:05:52 +0200

    renuprava modelu uzivatele

--- dj_Librapp/Librapp/models.py
+++ dj_Librapp/Librapp/models.py
@@ -18,11 +18,11 @@ class UzivatelManager(BaseUserManager):
         user.save()
         return user
 class Uzivatel(AbstractBaseUser):
-    username = models.CharField(max_length=200)
+    uzivatelske_jmeno = models.CharField(max_length=200)
     email = models.EmailField(max_length=300, unique=True)
     is_admin = models.BooleanField(default=False)
-    first_name = models.CharField(max_length=200)
-    last_name = models.CharField(max_length=200)
+    jmeno = models.CharField(max_length=200)
+    prijmeni = models.CharField(max_length=200)
 
     class Meta:
         verbose_name = "uživatel"
@@ -30,10 +30,10 @@ class Uzivatel(AbstractBaseUser):
 
     objects = UzivatelManager()
 
-    USERNAME_FIELD = "email"
+    USERNAME_FIELD = 'email'
 
     def __str__(self):
-        return "email: {}".format(self.email)
+        return self.jmeno + ' ' + self.prijmeni
 
     @property
     def is_staff(self):
@@ -59,7 +59,7 @@ class Kniha(models.Model):
     ISBN10 = models.CharField(max_length=180, blank=True)
     ISBN13 = models.CharField(max_length=180, blank=True)
     zanr = models.ForeignKey(Zanr, on_delete=models.SET_NULL, null= True, verbose_name="Žánr")
-    majitel = models.CharField(max_length=180, blank=True)
+    majitel = models.ForeignKey(Uzivatel, on_delete=models.CASCADE, verbose_name="Majitel")
 
     def __str__(self):
         return f"Název: {self.nazev} | Autor: {self.autor} | Žánr: {self.zanr}"
