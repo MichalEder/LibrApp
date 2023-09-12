baadcf7a8ac751b866a91b46c79e16d487e736d9
MichalEder <edermichal.eder@gmail.com>
2023-09-12 11:06:20 +0200

    Pridan form pro registraci

--- dj_Librapp/Librapp/forms.py
+++ dj_Librapp/Librapp/forms.py
@@ -10,3 +10,17 @@ class KnihaForm(forms.ModelForm):
 class VyhledavaniISBNForm(forms.Form):
     isbn = forms.CharField(label='ISBN', max_length=13, required=False)
 
+class RegistraceForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = Uzivatel
+        fields = ['email', 'password', 'jmeno', 'prijmeni', 'uzivatelske_jmeno']
+
+class LoginForm(forms.Form):
+    email = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        fields = ['email', 'password']
+
