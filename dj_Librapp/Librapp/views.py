6e138e37f35c91203d22d88b458cd14ad3479e29
MichalEder <edermichal.eder@gmail.com>
2023-09-14 09:46:32 +0200

    Pep8

--- dj_Librapp/Librapp/views.py
+++ dj_Librapp/Librapp/views.py
@@ -9,25 +9,24 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-
 # Create your views here.
 
 class UpravKnihu(generic.UpdateView):
-
     model = Kniha
     form = KnihaForm
     template_name = 'Librapp/uprav_knihu.html'
     fields = ['nazev', 'autor', 'rok_vydani', 'zanr', 'jazyk', 'ISBN10', 'ISBN13', 'majitel']
+
     def get_success_url(self):
         return reverse_lazy('detail', kwargs={'pk': self.object.id})
 
 
 class UpravProfil(generic.UpdateView):
-
     model = Uzivatel
     form = RegistraceForm
     template_name = 'Librapp/uprav_profil.html'
     fields = ['email', 'jmeno', 'prijmeni', 'uzivatelske_jmeno']
+
     def get_success_url(self):
         return reverse_lazy('profil', kwargs={'pk': self.object.id})
 
@@ -35,7 +34,7 @@ class UpravProfil(generic.UpdateView):
 class OdstranKnihu(generic.DeleteView):
     model = Kniha
     template_name = 'Librapp/odstran_knihu.html'
-    success_url = reverse_lazy('index')  # Redirect to the book list page after successful deletion
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -50,37 +49,35 @@ class OdstranKnihu(generic.DeleteView):
 class OdstranUzivatele(generic.DeleteView):
     model = Uzivatel
     template_name = 'Librapp/odstran_uzivatele.html'
-    success_url = reverse_lazy('index')  # Redirect to the book list page after successful deletion
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['confirmation_message'] = f"Opravdu si přejete smazat účet uživatele {self.object.jmeno} {self.object.prijmeni}?"
+        context[
+            'confirmation_message'] = f"Opravdu si přejete smazat účet uživatele {self.object.jmeno} {self.object.prijmeni}?"
         return context
 
 
 class Detail(generic.DetailView):
-
     model = Kniha
     template_name = 'Librapp/detail.html'
 
 
 class SeznamKnih(generic.ListView):
-
     template_name = "Librapp/index.html"
     context_object_name = "knihy"
-
 
     def get_queryset(self):
 
         query = self.request.GET.get('q')
         if query:
-            # If a search query is provided, filter books based on title using a case-insensitive search
             return Kniha.objects.filter(
                 Q(nazev__icontains=query) |
                 Q(autor__icontains=query)
             ).order_by('-id')
         else:
             return Kniha.objects.all().order_by('-id')
+
 
 class PridatKnihu(generic.edit.CreateView):
     form_class = KnihaForm
@@ -97,7 +94,7 @@ class PridatKnihu(generic.edit.CreateView):
     def post(self, request):
 
         search_form = self.form_search(request.POST)
-        book_form_empty = self.form_book(None)
+        book_form_empty: KnihaForm = self.form_book(None)
 
         if request.POST['action'] == 'Vyhledej knihu':
 
@@ -118,10 +115,12 @@ class PridatKnihu(generic.edit.CreateView):
                     }
 
                     book_form = KnihaForm(initial=initial_data)
-                    return render(request, f'Librapp/pridat_knihu.html', {'form': search_form, 'book_form': book_form})
+                    return render(request, f'Librapp/pridat_knihu.html',
+                                  {'form': search_form, 'book_form': book_form})
                 else:
                     search_form.add_error('isbn', 'ISBN nenalezeno')
-                    return render(request, f'Librapp/pridat_knihu.html', {'form': search_form, 'book_form': book_form_empty})
+                    return render(request, f'Librapp/pridat_knihu.html',
+                                  {'form': search_form, 'book_form': book_form_empty})
 
         elif request.POST['action'] == 'Ulož knihu':
             book_form = self.form_book(request.POST)
@@ -144,6 +143,7 @@ class PridatKnihu(generic.edit.CreateView):
                 book_info = data['items'][0]['volumeInfo']
                 return book_info
         return None
+
 
 class UzivatelViewLogin(generic.edit.CreateView):
     form_class = LoginForm
@@ -186,6 +186,7 @@ class UzivatelViewRegister(generic.edit.CreateView):
 
         return render(request, self.template_name, {"form": form})
 
+
 def logout_uzivatel(request):
     logout(request)
     return redirect('index')
@@ -201,14 +202,12 @@ class KnihyUzivatele(LoginRequiredMixin, generic.ListView):
 
         query = self.request.GET.get('q')
         if query:
-            # If a search query is provided, filter books based on title using a case-insensitive search
             return Kniha.objects.filter(
                 Q(nazev__icontains=query) |
-                Q(autor__icontains=query) ,
-                majitel=user  # Filter by the logged-in user
+                Q(autor__icontains=query),
+                majitel=user
             ).order_by('-id')
         else:
-            # Only show books owned by the logged-in user
             return Kniha.objects.filter(majitel=user).order_by('-id')
 
 
@@ -223,32 +222,26 @@ class Profil(LoginRequiredMixin, generic.TemplateView):
         context['user'] = user
 
         if query:
-            # If a search query is provided, filter books based on title using a case-insensitive search
+
             context['knihy'] = Kniha.objects.filter(
                 Q(nazev__icontains=query) |
-                Q(autor__icontains=query) ,
-                majitel=user  # Filter by the logged-in user
+                Q(autor__icontains=query),
+                majitel=user
             ).order_by('-id')
         else:
-            # Only show books owned by the logged-in user
             context['knihy'] = Kniha.objects.filter(majitel=user).order_by('-id')
 
         return context
 
 
-
-
 class SeznamUzivatelu(generic.ListView):
-
     template_name = "Librapp/uzivatele.html"
     context_object_name = "uzivatele"
-
 
     def get_queryset(self):
 
         query = self.request.GET.get('q')
         if query:
-            # If a search query is provided, filter books based on title using a case-insensitive search
             return Uzivatel.objects.filter(
                 Q(jmeno__icontains=query) |
                 Q(prijmeni__icontains=query) |
